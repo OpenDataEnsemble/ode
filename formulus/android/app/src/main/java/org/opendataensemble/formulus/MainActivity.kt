930b68f8f240e757845c1e5aa2e71e7c3b86cc5b
Emil Rossing <emil@rossing.org>
2026-03-15 09:58:27 +0100

    fix(formulus): Don't re-use factory from stale state

--- formulus/android/app/src/main/java/org/opendataensemble/formulus/MainActivity.kt
+++ formulus/android/app/src/main/java/org/opendataensemble/formulus/MainActivity.kt
@@ -1,11 +1,23 @@
 package org.opendataensemble.formulus
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 class MainActivity : ReactActivity() {
+
+  /**
+   * Prevents crash when app is opened after being killed in background (e.g. by OS).
+   * Android tries to restore ScreenFragment from saved state; react-native-screens
+   * requires this factory so fragments are not restored incorrectly.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
