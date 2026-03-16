71ecd92fae3ca0629fee56e16b7ad8158aefccf3
IamLRBA <jerrylarubafestus@gmail.com>
2026-03-17 00:42:58 +0300

    fix(formulus-formplayer): apply Prettier to updated components

--- formulus-formplayer/src/components/FormLayout.tsx
+++ formulus-formplayer/src/components/FormLayout.tsx
@@ -114,8 +114,8 @@ const FormLayout: React.FC<FormLayoutProps> = ({
         window.removeEventListener('resize', handleResize);
       };
     }
-  // initialViewportHeight is intentionally read only on mount
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // initialViewportHeight is intentionally read only on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
