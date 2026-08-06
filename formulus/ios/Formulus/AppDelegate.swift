5cb1035fce7a00e9c14603b8d2bcd6f01280191a
Christian Stærk <c@strong.dk>
2026-08-06 14:43:36 +0200

    feat(formulus): Added IOS metadata necessary for Apple AppStore submission

--- formulus/ios/Formulus/AppDelegate.swift
+++ formulus/ios/Formulus/AppDelegate.swift
@@ -9,25 +9,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
+  var launchOptions: [UIApplication.LaunchOptionsKey: Any]?
 
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    self.launchOptions = launchOptions
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
 
     reactNativeDelegate = delegate
     reactNativeFactory = factory
-
-    window = UIWindow(frame: UIScreen.main.bounds)
-
-    factory.startReactNative(
-      withModuleName: "Formulus",
-      in: window,
-      launchOptions: launchOptions
-    )
 
     return true
   }
